53695aff89112ce981505d6a7f0a81ca328e39b0
frizzly-mika <micael.augustons@hotmail.com>
2023-06-28 15:08:17 -0300

    Task 3 - Criação Model Projeto Spring

---
+++ src/main/java/com/generation/dumorro/DumorroApplication.java
@@ -0,0 +1,13 @@
+package com.generation.dumorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumorroApplication.class, args);
+	}
+
+}

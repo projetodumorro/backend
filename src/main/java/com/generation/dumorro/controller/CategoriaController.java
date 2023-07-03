bf5774c53fe2f1b7bab3678c012112f2d5c666ec
projetodumorro <projetodumorro@gmail.com>
2023-07-03 14:57:49 -0300

    Métodos Put e Delete

--- src/main/java/com/generation/dumorro/controller/CategoriaController.java
+++ src/main/java/com/generation/dumorro/controller/CategoriaController.java
@@ -1,17 +1,22 @@
 package com.generation.dumorro.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.dumorro.model.Categoria;
 import com.generation.dumorro.repository.CategoriaRepository;
@@ -48,5 +53,24 @@ public class CategoriaController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(categoriaRepository.save(categoria));
 	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria){
+		return categoriaRepository.findById(categoria.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(categoriaRepository.save(categoria)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		
+		if(categoria.isEmpty()) 
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		categoriaRepository.deleteById(id);
+		
+	}
 }
